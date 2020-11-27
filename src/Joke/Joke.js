ff38d057f07ae140e6a9720837dfdecbde464ab5
VioletaLunaR <vl.rodriguez10@uniandes.edu.co>
2020-11-27 17:36:24 -0500

    trae datos

--- src/Joke/Joke.js
+++ src/Joke/Joke.js
@@ -1,22 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, CardGroup , Card, ListGroup, ListGroupItem} from "react";
+const crypto = require("crypto");
 
 function Joke() {
 
     var [joke, setJoke] = useState();
-    var text;
+    var [characters, setCharacters] = useState();
+    var ts = "characters";
+    const url = 'https://gateway.marvel.com:443/v1/public/characters?apikey=';
+    const publicKey = 'a4e4b48439d12a8dd0df40f6a8f4c00d';
+    const privateKey = 'b95aacc55da1d587dc49da819e91bd8b39c68bb4';
+    const hash = crypto.createHash("md5").update(ts + privateKey + publicKey).digest('hex');
 
-    useEffect(()=>{
-        if(!navigator.onLine){
-            if(localStorage.getItem("joke") === null) {
-                setJoke("Loading...")
+
+
+    useEffect(() => {
+        if (!navigator.onLine) {
+            if (localStorage.getItem("characters") === null) {
+                setCharacters("Loading...")
             } else {
-                setJoke(localStorage.getItem("joke"));
+                setCharacters(localStorage.getItem("characters"));
             }
         } else {
-            const URL = "https://api.chucknorris.io/jokes/random";
-            fetch(URL).then(res=>res.json()).then(res=>{
-                setJoke(res.value);
-                localStorage.setItem("joke", res.value);
+            fetch(url + publicKey + '&hash=' + hash + '&ts=' + ts).then(res => res.json()).then(res => {
+                setCharacters(res.data.results);
+                // setJoke(res.value);
+                // localStorage.setItem("joke", res.value);
+                console.log(res.data.results);
             })
         }
     }, []);
@@ -24,13 +33,13 @@ function Joke() {
     return (
         <>
             <h1>
-                {text}
+                Characters:
             </h1>
-            <p>
-                {joke}
-            </p>
+
         </>
     );
+
 }
+
 
 export default Joke;
