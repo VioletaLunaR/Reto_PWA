e6ea8f89f558cbd1ee4282988f7ad22dff0a3233
VioletaLunaR <vl.rodriguez10@uniandes.edu.co>
2020-11-27 19:40:50 -0500

    Listo

--- src/Joke/Joke.js
+++ src/Joke/Joke.js
@@ -1,10 +1,15 @@
-import React, { useState, useEffect, CardGroup , Card, ListGroup, ListGroupItem} from "react";
+import React, { useState, useEffect, } from "react";
+import CardGroup from 'react-bootstrap/CardGroup';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+
 const crypto = require("crypto");
 
 function Joke() {
 
     var [joke, setJoke] = useState();
-    var [characters, setCharacters] = useState();
+    var [characters, setCharacters] = useState([]);
     var ts = "characters";
     const url = 'https://gateway.marvel.com:443/v1/public/characters?apikey=';
     const publicKey = 'a4e4b48439d12a8dd0df40f6a8f4c00d';
@@ -19,15 +24,17 @@ function Joke() {
                 setCharacters("Loading...")
             } else {
                 setCharacters(localStorage.getItem("characters"));
+
             }
         } else {
             fetch(url + publicKey + '&hash=' + hash + '&ts=' + ts).then(res => res.json()).then(res => {
                 setCharacters(res.data.results);
-                // setJoke(res.value);
-                // localStorage.setItem("joke", res.value);
+                localStorage.setItem("characters", res.data.results);
                 console.log(res.data.results);
-            })
+            });
         }
+
+
     }, []);
 
     return (
@@ -35,11 +42,39 @@ function Joke() {
             <h1>
                 Characters:
             </h1>
+            <CardGroup>
+                {characters.map(d => {
+                    return (
+                        <>
+                            <Card>
+                                {/* <Card.Img variant="top" src={d.thumbnail + 'apikey=' + publicKey + '&hash=' + hash + '&ts=' + ts} /> */}
+                                <Card.Body>
+                                    <Card.Title>{d.name} </Card.Title>
+                                    <Card.Text>
+                                        {d.description}
+                                    </Card.Text>
+                                </Card.Body>
+                                {/* <ListGroup className="list-group-flush">
+                                    <ListGroupItem>Cras justo odio</ListGroupItem>
+                                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                                </ListGroup> */}
+
+                            </Card>
+
+                        </>
+                    )
+                }
+                )}
+            </CardGroup>
 
         </>
     );
 
-}
+  
 
+
+
+}
 
 export default Joke;
